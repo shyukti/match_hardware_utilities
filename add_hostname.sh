10952ff5ccd9fef454194bf91a6a0863860b3b95
MiRgus <47657974+matchRos@users.noreply.github.com>
2023-01-06 12:33:31 +0100

    Add files via upload

---
+++ add_hostname.sh
@@ -0,0 +1,9 @@
+host="$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1') $(hostname)"  
+
+
+
+ssh rosmatch@mur "sudo -- sh -c 'echo $host >> /etc/hosts'"
+
+
+
+
